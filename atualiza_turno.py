4e198480c8588738e73a83dbe752e57e38e2665b
Hugo <hugovasantos@gmail.com>
2022-10-10 07:40:17 -0300

    versão nova do atualiza turno

--- atualiza_turno.py
+++ atualiza_turno.py
@@ -1,108 +1,99 @@
 import requests
 import pandas as pd
-
-from datetime import datetime
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
-from read_sheets import read_sheets
-from write_sheets import write_sheets
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://u369946143_pcpBahia:#Energia26#90@31.220.16.3/u369946143_pcpBahia", echo=True)
+
+abertura_turnos = pd.read_sql_table('abertura_turnos', con=engine)
+turnos_registrados = abertura_turnos['cod_turno_tur']
+
+# COLETA INFORMAÇÕES DO GPM
+url = "https://sirtecba.gpm.srv.br/gpm/geral/consulta_turno.php?tip=C"
+headers = {
+    'cookie': "PHPSESSID=ubmq5m8thditko9gdll622bh32"
+}
+dia = datetime.now()
+diaehora = dia.strftime("%d/%m/%Y %H:%M")
+dia = dia.strftime("%d/%m/%Y")
+data = {
+    "data_inicial": str(dia),
+    "data_final": str(dia),
+    "submit": "Pesquisar",
+    "contrato": "11",
+    "h_tot": "999",
+    "avancaTodos": "Todas",
+}
+
+planilha = []
+
+#Busca turnos do contrato final 70
+try:
+    resposta = requests.post(url, headers=headers, data=data) 
+    conteudo = resposta.content
+    site = BeautifulSoup(conteudo, "html.parser") #Captura o html da página
+except:
+    print('Não foi possível fazer conexão com o GPM.')
+try:    
+    tabela = site.find('table', attrs = {"class": "tbl_100_c_5p_0h_lin zebra"})
+    linhas = tabela.findAll('tr')
+    linhas.pop(0)
+except:
+    print('Nenhum turno aberto.')
 
 
-def atualiza_turno():
-    hora = datetime.now()
-    hora = hora.strftime('%H:%M')
-    print('[' + hora + ']', "Atualizando abertura de turnos...")
+agora = datetime.now()
+oito_e_dez = agora.replace(hour=8, minute=10, second=0, microsecond=0)
 
-    id='1kthDa5_Ed7mfpnye-xpnTzZXkFnBB4veVyxeM9Ti8ds'
-    range="turnos!A:A"
-    result = read_sheets(id, range, 'FORMATTED_VALUE')
-    df = pd.DataFrame(result, columns = result.pop(0))
-    tamanhoTabela = len(df['cod_turno_tur'])
+for i in linhas:
+    cont = 0
+    colunas = i.findAll('td')
+    if colunas[3].text in turnos_registrados.values:
+        continue
+    else:
+        hora_abertura = datetime.strptime(colunas[12].text, '%d/%m/%Y %H:%M:%S')
+        if hora_abertura > oito_e_dez:
+            pontualidade = 0
+        elif hora_abertura < oito_e_dez:
+            pontualidade = 1
 
-    # COLETA INFORMAÇÕES DO GPM
-    url = "https://sirtecba.gpm.srv.br/gpm/geral/consulta_turno.php?tip=C"
-    headers = {
-        'cookie': "PHPSESSID=ubmq5m8thditko9gdll622bh32"
-    }
-    dia = datetime.now()
-    diaehora = dia.strftime("%d/%m/%Y %H:%M")
-    dia = dia.strftime("%d/%m/%Y")
-    data = {
-        "data_inicial": str(dia),
-        "data_final": str(dia),
-        "submit": "Pesquisar",
-        "contrato": "11",
-        "h_tot": "999",
-        "avancaTodos": "Todas",
-    }
+        planilha.append([colunas[3].text, colunas[7].text, colunas[12].text, pontualidade])
 
-    #Busca turnos do contrato final 70
-    try:
-        resposta = requests.post(url, headers=headers, data=data) 
-        conteudo = resposta.content
-        site = BeautifulSoup(conteudo, "html.parser") #Captura o html da página
-    except:
-        print('Não foi possível fazer conexão com o GPM.')
-    try:    
-        tabela = site.find('table', attrs = {"class": "tbl_100_c_5p_0h_lin zebra"})
-        linhas = tabela.findAll('tr')
-        linhas.pop(0)
-    except:
-        print('Nenhum turno aberto.')
-        return
+#Busca turnos do contrato final 69
+try:
+    data["contrato"]= "12"
+    resposta = requests.post(url, headers=headers, data=data) 
+    conteudo = resposta.content
+    site = BeautifulSoup(conteudo, "html.parser") #Captura o html da página
+except:
+    print('Não foi possível fazer conexão com o GPM.')
+try:
+    tabela = site.find('table', attrs = {"class": "tbl_100_c_5p_0h_lin zebra"})
+    linhas = tabela.findAll('tr')
+    linhas.pop(0)
+except:
+    print('Nenhum turno aberto.')
+
+for i in linhas:
+    cont = 0
+    colunas = i.findAll('td')
+    if colunas[3].text in turnos_registrados.values:
+        continue
+    else:
+        hora_abertura = datetime.strptime(colunas[12].text, '%d/%m/%Y %H:%M:%S')
+        if hora_abertura > oito_e_dez:
+            pontualidade = 0
+        elif hora_abertura < oito_e_dez:
+            pontualidade = 1
+
+        planilha.append([colunas[3].text, colunas[7].text, colunas[12].text, pontualidade])
     
 
-    planilha = []
+df = pd.DataFrame(planilha, columns=['cod_turno_tur','des_equipe', 'dta_solicitacao', 'pontualidade'])
+df['dta_solicitacao'] = pd.to_datetime(df['dta_solicitacao'], format='%d/%m/%Y %H:%M:%S')
 
-    for i in linhas:
-        cont = 0
-        colunas = i.findAll('td')
-        for n in df['cod_turno_tur']:
-            if colunas[3].text == n:
-                cont += 1
-        if cont == 0:
-            planilha.append([colunas[3].text, colunas[7].text, colunas[12].text])
-
-    #Busca turnos do contrato final 69
-    try:
-        data["contrato"]= "12"
-        resposta = requests.post(url, headers=headers, data=data) 
-        conteudo = resposta.content
-        site = BeautifulSoup(conteudo, "html.parser") #Captura o html da página
-    except:
-        print('Não foi possível fazer conexão com o GPM.')
-    try:
-        tabela = site.find('table', attrs = {"class": "tbl_100_c_5p_0h_lin zebra"})
-        linhas = tabela.findAll('tr')
-        linhas.pop(0)
-    except:
-        print('Nenhum turno aberto.')
-        return
-
-    for i in linhas:
-        cont = 0
-        colunas = i.findAll('td')
-        for n in df['cod_turno_tur']:
-            if colunas[3].text == n:
-                cont += 1
-        if cont == 0:
-            planilha.append([colunas[3].text, colunas[7].text, colunas[12].text])
+print(df)
 
 
-
-    # PREENCHIMENTO DA PLANILHA ONLINE
-
-    id='1kthDa5_Ed7mfpnye-xpnTzZXkFnBB4veVyxeM9Ti8ds'
-    range = ("turnos!A"+str(tamanhoTabela + 2))
-    valores = planilha
-    write_sheets(id, range, valores)
-
-    range = ("turnos!B3")
-    valores = [[str(diaehora)]]
-    write_sheets(id, range, valores)
-    hora = datetime.now()
-    hora = hora.strftime('%H:%M')
-    print('[' + hora + ']' , len(planilha), "aberturas de turno foram adicionadas")
-
-
-if __name__ == "__main__":
-    atualiza_turno()
+df.to_sql('abertura_turnos', index=False, if_exists='append', con=engine)
