06a0abcb5d010fb20bb6c44e92dbde9b3ddcf23e
Hugo <hugovasantos@gmail.com>
2023-01-02 19:30:50 -0300

    adicionado equipes do comercial na abertura de turnos

--- atualiza_turno.py
+++ atualiza_turno.py
@@ -4,6 +4,9 @@ from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 
+global planilha
+planilha = []
+
 def busca_n_checklist(cod_turno, cookie):
     url = "https://sirtecba.gpm.srv.br/gpm/geral/relatorio_turno.php"
     headers = {
@@ -61,17 +64,11 @@ def possui_camera(cod_turno, cookie):
     else:
         return('checklist incorreto')
 
-
-
-def atualiza_turno():
-    cookie = "PHPSESSID=26qak1aargp7f8pmbu7gjev40k"
-
+def coleta_infos(cod_contrato, planilha):
     engine = create_engine("mysql+pymysql://u369946143_pcpBahia:#Energia26#90@31.220.16.3/u369946143_pcpBahia", echo=False)
-
     abertura_turnos = pd.read_sql_table('abertura_turnos', con=engine)
+    cookie = "PHPSESSID=26qak1aargp7f8pmbu7gjev40k"
     turnos_registrados = abertura_turnos['cod_turno_tur']
-
-    # COLETA INFORMAÇÕES DO GPM
     url = "https://sirtecba.gpm.srv.br/gpm/geral/consulta_turno.php?tip=C"
     headers = {
         'cookie': cookie
@@ -82,12 +79,10 @@ def atualiza_turno():
         "data_inicial": str(dia),
         "data_final": str(dia),
         "submit": "Pesquisar",
-        "contrato": "11",
+        "contrato": cod_contrato,
         "h_tot": "999",
         "avancaTodos": "Todas",
     }
-
-    planilha = []
 
     #Busca turnos do contrato final 70
     try:
@@ -119,44 +114,22 @@ def atualiza_turno():
                 pontualidade = 1
 
             resposta = possui_camera(colunas[3].text, cookie)
+            planilha.append([colunas[3].text, colunas[7].text, colunas[12].text, pontualidade, resposta, colunas[11].text])
 
-            planilha.append([colunas[3].text, colunas[7].text, colunas[12].text, pontualidade, resposta])
+def atualiza_turno():
+    
+    coleta_infos("12", planilha)
+    coleta_infos("11", planilha)
+    coleta_infos("16", planilha)
+    coleta_infos("15", planilha)
+    coleta_infos("13", planilha)
 
-    #Busca turnos do contrato final 69
-    try:
-        data["contrato"]= "12"
-        response = requests.post(url, headers=headers, data=data) 
-        conteudo = response.content
-        site = BeautifulSoup(conteudo, "html.parser") #Captura o html da página
-    except:
-        print('Não foi possível fazer conexão com o GPM.')
-    try:
-        tabela = site.find('table', attrs = {"class": "tbl_100_c_5p_0h_lin zebra"})
-        linhas = tabela.findAll('tr')
-        linhas.pop(0)
-    except:
-        print('Nenhum turno aberto.')
-
-    for i in linhas:
-        colunas = i.findAll('td')
-        if colunas[3].text in turnos_registrados.values:
-            continue
-        else:
-            hora_abertura = datetime.strptime(colunas[12].text, '%d/%m/%Y %H:%M:%S')
-            if hora_abertura > oito_e_dez:
-                pontualidade = 0
-            elif hora_abertura < oito_e_dez:
-                pontualidade = 1
-                
-            resposta = possui_camera(colunas[3].text, cookie)
-            
-            planilha.append([colunas[3].text, colunas[7].text, colunas[12].text, pontualidade, resposta])
-
-    df = pd.DataFrame(planilha, columns=['cod_turno_tur','des_equipe', 'dta_solicitacao', 'pontualidade', 'possui_camera'])
+    df = pd.DataFrame(planilha, columns=['cod_turno_tur','des_equipe', 'dta_solicitacao', 'pontualidade', 'possui_camera', 'placa_veiculo'])
     df['dta_solicitacao'] = pd.to_datetime(df['dta_solicitacao'], format='%d/%m/%Y %H:%M:%S')
 
     print(df)
 
+    engine = create_engine("mysql+pymysql://u369946143_pcpBahia:#Energia26#90@31.220.16.3/u369946143_pcpBahia", echo=False)
 
     df.to_sql('abertura_turnos', index=False, if_exists='append', con=engine)
 
