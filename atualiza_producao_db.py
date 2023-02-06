67d996d13313cc7486d1dfdebc82621f5571ebe8
Hugo <hugovasantos@gmail.com>
2023-02-06 07:34:11 -0300

    atualização de autenticador gpm

--- atualiza_producao_db.py
+++ atualiza_producao_db.py
@@ -126,7 +126,7 @@ def buscaRelatorioServico(codigo_serv, cookie):
     return(lista_atividades)    
 
 def atualiza_producao_db():
-    cookie = 'PHPSESSID=f8262q9965s3ab2lo8f46k11mc'
+    cookie = 'PHPSESSID=u01g0ne0d4l8ja128fbihccq7o'
 
     agora = datetime.now()
     dia = agora.date()
